fded3540396c80f95b46b8eb867639cf53e7c521
steven wang <stevenwang@hotmail.co.nz>
2017-04-02 20:37:16 +1200

    2:00:38

--- src/app/article.ts
+++ src/app/article.ts
@@ -1,5 +1,26 @@
+interface ArticleJSON{
+  title: string;
+  url: string;
+  votes: number;
+  publishedAt: string;
+  description: string;
+  author: string;
+} 
+
 export class Article {
   public publishedAt: Date;
+
+
+  static fromJSON(json: ArticleJSON): Article {
+    let article = Object.create(Article.prototype);
+    return Object.assign(article, json,{
+      votes: json.votes? json.votes:0,
+      publishedAt: json.publishedAt ?
+                    new Date(json.publishedAt):
+                    new Date()
+    } );
+  }
+
 
   constructor(
     public title: string,
