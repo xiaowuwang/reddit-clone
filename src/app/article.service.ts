b7850aec4aff065c1a7371e856b3bee6436ba5b4
steven wang <stevenwang@hotmail.co.nz>
2017-04-02 20:02:56 +1200

    add http, 1:49:50

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,30 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Article } from "app/article";
+import { Http, URLSearchParams } from "@angular/http";
+import 'rxjs/add/operator/toPromise';
+
+const baseUrl ='https://newsapi.org';
+const  newsApiKey ='c29bfb1e76054a40bd3dadbeac324a29';
 
 @Injectable()
 export class ArticleService {
-  constructor() { }
+  constructor(
+    private http: Http
+  ) { }
   public getArticles(): Promise<Article[]> {
-    return new Promise(resolve=>{
-      setTimeout(()=>{
-        resolve([
-          new Article(
-            'The Angular 2 screencast',
-            'The easiest way to learn Angular 2 is with Fullstack.io!',
-            10
-          ),
-          new Article(
-            'The Angular 3 screencast',
-            'The easiest way to learn Angular 3 is with Fullstack.io!',
-            8
-          ),
-          new Article(
-            'The Angular 4 screencast',
-            'The easiest way to learn Angular 4 is with Fullstack.io!',
-            6
-          )
-        ])  
-    },1000);
-  });
-}
+    let params = new URLSearchParams();
+    params.set('apiKey',newsApiKey);
+    params.set('source', 'reddit-r-all');
+    return this.http
+              .get(`${baseUrl}/v1/articles`,{
+                search: params
+              })
+              .toPromise()
+              .then(r=> r.json())
+              .then(json=>json.articles)
+              .then(articles=>{
+                console.log('json->', articles);
+                return articles;
+              })
+              .catch(err=>{
+                console.log('we got na error', err);
+              });
+
+
+
+              }
 }
