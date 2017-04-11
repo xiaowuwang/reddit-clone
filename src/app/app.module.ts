33506b2611fe206ec5cd51c4bbe82fd27a1fcf0e
steven wang <stevenwang@hotmail.co.nz>
2017-04-11 18:42:05 +1200

    fix router issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { appRoutes } from './app.routes';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ArticleComponent } from './article/article.component';
@@ -12,7 +11,24 @@ import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleService } from './article.service';
 import { ArticleListHeaderComponent } from './article-list-header/article-list-header.component';
 import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes, PreloadAllModules } from "@angular/router";
+import { LocationStrategy, HashLocationStrategy } from "@angular/common";
 
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'news/reddit-r-all',
+    pathMatch: 'full'
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'news/:sourceKey',
+    component: ArticleListComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -27,10 +43,11 @@ import { AboutComponent } from './about/about.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    appRoutes
+    RouterModule.forRoot(routes, { useHash: true, preloadingStrategy: PreloadAllModules })  // .../#/crisis-center/  
   ],
   providers: [
-    ArticleService
+    ArticleService,
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ],
   bootstrap: [AppComponent]
 })
